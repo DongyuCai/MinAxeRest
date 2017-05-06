c2594e1ffa3d5b84f3e03517beefed4da7c51d9e
DongyuCai <1157656909@qq.com>
2017-05-06 10:47:17 +0800

    clean

--- frontend/js/appMain/base.js
+++ frontend/js/appMain/base.js
@@ -1,7 +1,7 @@
 // /************************** DEV ******************************************/
 
 
-var API_BASE_DOMAIN = 'http://localhost:8090';
+var API_BASE_DOMAIN = 'http://localhost:8080/api';
 var API_BASE_URL = API_BASE_DOMAIN+'/sys';
 
 // var API_BASE_URL = 'http://test2.sl1288.com';
